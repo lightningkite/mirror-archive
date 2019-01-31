d2d3c9348a1609631cb500fa83974c39af394ad8
Joseph Ivie <josephivie@gmail.com>
2019-01-31 14:43:16 -0700

    UI Kit

---
+++ mirror-archive-nitrite/src/test/kotlin/com/lightningkite/kotlinx/persistence/TestRegistry.kt
@@ -0,0 +1,10 @@
+package com.lightningkite.kotlinx.persistence
+
+import com.lightningkite.kommon.native.SharedImmutable
+import com.lightningkite.mirror.info.*
+import kotlin.reflect.KClass
+
+@SharedImmutable
+val TestRegistry = ClassInfoRegistry(
+
+)
